f6814c1ff0f9406bfe99a19592ea3d84c21d6fb1
Gabriel <gabrielecheverria1993@gmail.com>
2024-05-05 19:51:12 -0300

    add features to remaining buttons and cart and made it responsive

--- index.js
+++ index.js
@@ -11,21 +11,21 @@ const productCatalog = [
         name: "PC Armada Gamer Extreme Ryzen 7 5700 /Cooler /RTX 4070 Super /B550M /16GB /1TB /Gabinete /Fuente 750W 80P",
         price: 1775648,
         stock: 5,
-        image: "product1.webp",    
+        image: "product1.webp",
     },
     {
         id: 3,
         name: "PC Armada Gamer Extreme I5 13400F /Cooler /RTX 4070 Super /B760M /16GB /500GB /Gabinete /Fuente 750W 80P",
         price: 1833569,
         stock: 5,
-        image: "product1.webp",    
+        image: "product1.webp",
     },
     {
         id: 4,
         name: "PC Armada Gamer Extreme I7 12700F /Cooler /RTX 4070 Super /B760M /16GB /500GB /Gabinete /Fuente 750W 80P",
         price: 1952423,
         stock: 5,
-        image: "product1.webp",    
+        image: "product1.webp",
     },
     {
         id: 5,
@@ -39,40 +39,44 @@ const productCatalog = [
         name: `Notebook Gamer Lenovo Legion 5 15ACH6H  FHD 15.6" R5 5600H 16GB (2x8GB) 1TB SSD NVME RTX 3060 6GB W11S 165Hz`,
         price: 1295750,
         stock: 5,
-        image: "product2.jpg",    
+        image: "product2.jpg",
     },
     {
         id: 7,
         name: `Notebook Gamer Lenovo Legion 5 15ACH6H WQHD 2K 15.6" R5 5600H 16GB (2x8GB) 512GB NVME RTX 3060 6GB W11S 165Hz`,
         price: 1199990,
         stock: 5,
-        image: "product2.jpg",    
+        image: "product2.jpg",
     },
     {
         id: 8,
         name: `Notebook Gamer Lenovo Legion 5 15ACH6A WQHD 2K 15.6" R5 5600H 16GB (2x8GB) 512GB SSD NVME RX6600M 8GB W11 165Hz Silver`,
         price: 1169850,
         stock: 5,
-        image: "product2.jpg",    
+        image: "product2.jpg",
     },
     {
         id: 9,
         name: `Notebook Lenovo ThinkPad E14 FHD 14" Core I7 1165G7 8GB 256GB SSD NVMe Freedos`,
         price: 1144990,
         stock: 5,
-        image: "product2.jpg",    
+        image: "product2.jpg",
     },
     {
         id: 10,
         name: `Notebook Gamer Lenovo Legion 5 15ITH6H FHD 15.6" I5 11400H 8GB 512GB NVME RTX 3060 6GB W11S 165Hz Silver`,
         price: 1134990,
         stock: 5,
-        image: "product2.jpg",    
+        image: "product2.jpg",
     },
 ];
 
-const initializeProducts = (products) => {
+let isCartOpen = false;
+
+// Mostrar la lista de productos
+const showProductList = (products) => {
     const productsContainer = document.getElementById("products-container");
+    productsContainer.innerHTML = "";
 
     products.forEach(product => {
         let article = document.createElement("article");
@@ -89,6 +93,8 @@ const initializeProducts = (products) => {
         let addToCartButton = document.createElement("button")
         addToCartButton.className = "add-to-cart-btn";
         addToCartButton.textContent = "Añadir al carrito";
+        let productStock = checkProductStock(product.id);
+        if (!productStock) addToCartButton.disabled = true;
         addToCartButton.dataset.productId = product.id;
         productContent.appendChild(productImg);
         productContent.appendChild(productTitle);
@@ -100,6 +106,7 @@ const initializeProducts = (products) => {
     setupProductsEvents();
 };
 
+// Mostrar el carrito de compras con los productos agregados
 const showCart = () => {
     const cartList = document.getElementById("cart-list");
     cartList.innerHTML = "";
@@ -128,6 +135,8 @@ const showCart = () => {
         let addItemButton = document.createElement("button");
         addItemButton.className = "btn-add";
         addItemButton.dataset.productId = cartProduct.id;
+        let productStock = checkProductStock(cartProduct.id);
+        if (!productStock) addItemButton.disabled = true;
         let addSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         addSvg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
         addSvg.setAttribute("width", "24");
@@ -170,13 +179,70 @@ const showCart = () => {
         cartProductItem.appendChild(article);
         cartList.appendChild(cartProductItem);
     });
+
+    // Elemento para mostrar el precio total
+    let totalPrice = checkTotalPrice();
+    let totalElement = document.createElement("div");
+    totalElement.textContent = "Total:";
+    let totalAmountElement = document.createElement("strong");
+    totalAmountElement.textContent = `$${totalPrice}`;
+    totalElement.appendChild(totalAmountElement);
+    totalElement.className = "total-price";
+    // Botones
+    let buyButton = document.createElement("button");
+    buyButton.className = "btn-buy";
+    buyButton.textContent = "Comprar";
+    let clearCartButton = document.createElement("button");
+    clearCartButton.className = "btn-clear";
+    clearCartButton.textContent = "Limpiar carrito";
+
+    // Agregar precio total y botonoes al final de la lista del carrito
+    cartList.appendChild(totalElement);
+    cartList.appendChild(buyButton);
+    cartList.appendChild(clearCartButton);
+
     setupCartEvents();
 };
 
+// Obtener los productos del carrito almacenados en localStorage
 const getStoredProducts = () => {
     return localStorage.getItem("cart");
 };
 
+// Mostrar o ocultar el carrito al hacer click
+const toggleCart = () => {
+    const cart = document.getElementById('cart');
+    const productsContainer = document.getElementById('products-container');
+    const logoContainer = document.getElementById('cart-toggle-icon');
+    const cartProductList = document.getElementById('cart-list');
+    if (isCartOpen) {
+        productsContainer.className = 'products-container-cart-closed';
+        cart.className = "cart-closed";
+        logoContainer.className = 'logo-cart-is-closed';
+        logoContainer.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 16 16">
+            <path tColor" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607L1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4a2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4a2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2a1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2a1 1 0 0 1 0-2"
+            />
+        </svg>
+        `;
+        cartProductList.style.display = 'none';
+        isCartOpen = false;
+    } else {
+        productsContainer.className = 'products-container-cart-open';
+        cart.className = "cart-open";
+        logoContainer.className = 'logo-cart-is-open';
+        logoContainer.innerHTML = `
+        <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 16 16">
+            <path fill="#000000" d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607l1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4a2 2 0 0 0 0-4h7a2 2 0 1 0 0 4a2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1zM6 14a1 1 0 1 1-2 0a1 1 0 0 1 2 0m7 0a1 1 0 1 1-2 0a1 1 0 0 1 2 0M7.354 5.646L8.5 6.793l1.146-1.147a.5.5 0 0 1 .708.708L9.207 7.5l1.147 1.146a.5.5 0 0 1-.708.708L8.5 8.207L7.354 9.354a.5.5 0 1 1-.708-.708L7.793 7.5L6.646 6.354a.5.5 0 1 1 .708-.708"
+            />
+        </svg>
+        `;
+        cartProductList.style.display = 'flex';
+        isCartOpen = true;
+    }
+};
+
+// Agregar un producto al carrito
 const addToCart = (id) => {
     const storedProducts = getStoredProducts();
     let cartProductsArray = [];
@@ -198,13 +264,14 @@ const addToCart = (id) => {
     // Si no hay productos en el carrito y este sera el primero o 
     // no hay un item de este producto 
     newCartProduct = {
-        ...product, 
+        ...product,
         count: 1,
     }
     let newCartProducts = [...cartProductsArray, newCartProduct];
     localStorage.setItem('cart', JSON.stringify(newCartProducts));
 };
 
+// Quitar un producto del carrito
 const removeFromCart = (id) => {
     const storedProducts = getStoredProducts();
     if (!storedProducts) return;
@@ -226,24 +293,64 @@ const removeFromCart = (id) => {
     localStorage.setItem('cart', JSON.stringify(newCartProducts));
 };
 
+// Encontrar un producto por su ID dentro de un array de productos
 const findProductById = (id, productsArray) => {
     return productsArray.find((product) => product.id === id);
 };
 
+// Chequear si un producto tiene stock
 const checkProductStock = (id) => {
     let product = findProductById(id, productCatalog);
-    if (!product) return;
-    if (product.stock <= 0) return;
+    if (!product) return false;
+    if (product.stock <= 0) return false;
     const storedProducts = getStoredProducts();
-    if (!storedProducts) return;
+    if (!storedProducts) return true;
     let cartProductsArray = JSON.parse(storedProducts);
-    if (!cartProductsArray.length) return;
+    if (!cartProductsArray.length) return true;
     let existingProductInCart = findProductById(id, cartProductsArray);
-    if (!existingProductInCart) return;
+    if (!existingProductInCart) return true;
     let stock = (existingProductInCart.count < product.stock);
     return stock;
 };
 
+// Chequear el precio total del carrito
+const checkTotalPrice = () => {
+    const storedProducts = getStoredProducts();
+    if (!storedProducts) return 0;
+    const cartProducts = JSON.parse(storedProducts);
+    if (!cartProducts.length) return 0;
+
+    let totalPrice = 0;
+
+    cartProducts.forEach(cartProduct => {
+        totalPrice += cartProduct.price * cartProduct.count;
+    });
+
+    return totalPrice;
+};
+
+// Limpiar carrito
+const clearCart = () => {
+    localStorage.removeItem("cart");
+    showCart();
+    showProductList(productCatalog);
+};
+
+// Función para realizar la simulación de la compra
+const performPurchase = () => {
+    const storedProducts = getStoredProducts();
+    if (!storedProducts) return;
+    const cartProducts = JSON.parse(storedProducts);
+    if (!cartProducts.length) return;
+
+    let totalPrice = checkTotalPrice();
+
+    // Se muestra una alerta con un mensaje para indicar que se realizó con éxito
+    alert(`Has realizado una compra con un valor de $${totalPrice}, ¡Felicitaciones!`);
+    clearCart();
+};
+
+// Eventos del DOM para el listado de productos
 const setupProductsEvents = () => {
     let addToCartButtons = document.querySelectorAll(".add-to-cart-btn");
     if (addToCartButtons?.length) {
@@ -254,12 +361,13 @@ const setupProductsEvents = () => {
                 if (!btnProductId) return;
                 addToCart(Number(btnProductId));
                 showCart();
-                btn.disabled = true;
+                showProductList(productCatalog);
             });
         });
     }
 };
 
+// Eventos del DOM para el carrito de compras
 const setupCartEvents = () => {
     let addItemButtons = document.querySelectorAll(".btn-add");
     if (addItemButtons?.length) {
@@ -270,6 +378,7 @@ const setupCartEvents = () => {
                 if (!btnProductId) return;
                 addToCart(Number(btnProductId));
                 showCart();
+                showProductList(productCatalog);
             });
         });
     };
@@ -283,12 +392,26 @@ const setupCartEvents = () => {
                 if (!btnProductId) return;
                 removeFromCart(Number(btnProductId));
                 showCart();
+                showProductList(productCatalog);
             });
         });
     };
+
+    let clearCartButton = document.querySelector(".btn-clear");
+    if (clearCartButton) {
+        clearCartButton.addEventListener("click", clearCart);
+    }
+
+    let buyButton = document.querySelector(".btn-buy");
+    if (buyButton) {
+        buyButton.addEventListener("click", performPurchase);
+    }
 };
 
+// Funciones iniciales a ejecutar cuando carga la página
 document.addEventListener("DOMContentLoaded", () => {
-    initializeProducts(productCatalog);
+    showProductList(productCatalog);
     showCart();
+    const cartLogo = document.getElementById('cart-toggle-icon');
+    cartLogo.addEventListener('click', toggleCart);
 });
