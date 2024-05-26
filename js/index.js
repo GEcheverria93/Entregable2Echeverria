8a37357c1a8a7faefff27a42d2899a3a63df06ab
Gabriel <gabrielecheverria1993@gmail.com>
2024-05-26 18:36:48 -0300

    move products catalog json

--- js/index.js
+++ js/index.js
@@ -1,75 +1,17 @@
-const productCatalog = [
-    {
-        id: 1,
-        name: "PC Armada Gamer Extreme Ryzen 5 5600 /Cooler /RTX 4070 Super /B550M /32GB /1TB /Gabinete /Fuente 750W 80P",
-        price: 1763236,
-        stock: 5,
-        image: "product1.webp",
-    },
-    {
-        id: 2,
-        name: "PC Armada Gamer Extreme Ryzen 7 5700 /Cooler /RTX 4070 Super /B550M /16GB /1TB /Gabinete /Fuente 750W 80P",
-        price: 1775648,
-        stock: 5,
-        image: "product1.webp",
-    },
-    {
-        id: 3,
-        name: "PC Armada Gamer Extreme I5 13400F /Cooler /RTX 4070 Super /B760M /16GB /500GB /Gabinete /Fuente 750W 80P",
-        price: 1833569,
-        stock: 5,
-        image: "product1.webp",
-    },
-    {
-        id: 4,
-        name: "PC Armada Gamer Extreme I7 12700F /Cooler /RTX 4070 Super /B760M /16GB /500GB /Gabinete /Fuente 750W 80P",
-        price: 1952423,
-        stock: 5,
-        image: "product1.webp",
-    },
-    {
-        id: 5,
-        name: "PC Armada Gamer Extreme I9 14900K /Cooler /RX 7900 XTX /Z790 /32GB /1TB /Gabinete /Fuente 850W 80P",
-        price: 3672260,
-        stock: 5,
-        image: "product1.webp",
-    },
-    {
-        id: 6,
-        name: `Notebook Gamer Lenovo Legion 5 15ACH6H  FHD 15.6" R5 5600H 16GB (2x8GB) 1TB SSD NVME RTX 3060 6GB W11S 165Hz`,
-        price: 1295750,
-        stock: 5,
-        image: "product2.jpg",
-    },
-    {
-        id: 7,
-        name: `Notebook Gamer Lenovo Legion 5 15ACH6H WQHD 2K 15.6" R5 5600H 16GB (2x8GB) 512GB NVME RTX 3060 6GB W11S 165Hz`,
-        price: 1199990,
-        stock: 5,
-        image: "product2.jpg",
-    },
-    {
-        id: 8,
-        name: `Notebook Gamer Lenovo Legion 5 15ACH6A WQHD 2K 15.6" R5 5600H 16GB (2x8GB) 512GB SSD NVME RX6600M 8GB W11 165Hz Silver`,
-        price: 1169850,
-        stock: 5,
-        image: "product2.jpg",
-    },
-    {
-        id: 9,
-        name: `Notebook Lenovo ThinkPad E14 FHD 14" Core I7 1165G7 8GB 256GB SSD NVMe Freedos`,
-        price: 1144990,
-        stock: 5,
-        image: "product2.jpg",
-    },
-    {
-        id: 10,
-        name: `Notebook Gamer Lenovo Legion 5 15ITH6H FHD 15.6" I5 11400H 8GB 512GB NVME RTX 3060 6GB W11S 165Hz Silver`,
-        price: 1134990,
-        stock: 5,
-        image: "product2.jpg",
-    },
-];
+let productCatalog = [];
+
+const getProducts = async () => {
+    try {
+        const productResponse = await fetch("./js/products.json");
+        if (!productResponse.ok) 
+            throw new Error("La respuesta no fue exitosa. Status: " + productResponse.statusText);
+
+        return productResponse.json();
+    } catch (error) {
+        console.error("Hubo un error en la petición:", error.message);
+    }
+};
+
 
 let isCartOpen = false;
 
@@ -409,7 +351,8 @@ const setupCartEvents = () => {
 };
 
 // Funciones iniciales a ejecutar cuando carga la página
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
+    productCatalog = await getProducts();
     showProductList(productCatalog);
     showCart();
     const cartLogo = document.getElementById('cart-toggle-icon');
